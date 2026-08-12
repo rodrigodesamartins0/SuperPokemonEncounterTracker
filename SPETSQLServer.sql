b0a9c9ba870972732a8d31dec18420dde4f8200c
rodrigodesamartins0 <rodrigodesamartins0@gmail.com>
2026-08-12 20:49:03 -0300

    Quinta Geração
    Quinta geração de Pokémon (#494-#649) adicionada a tabela da National Pokedex

--- SPETSQLServer.sql
+++ SPETSQLServer.sql
@@ -96,7 +96,18 @@ VALUES ('Bulbasaur'),('Ivysaur'),('Venusaur'),('Charmander'),('Charmeleon'),('Ch
 ('Gible'),('Gabite'),('Garchomp'),('Munchlax'),('Riolu'),('Lucario'),('Hippopotas'),('Hippowdon'),('Skorupi'),('Drapion'),('Croagunk'),('Toxicroak'),('Carnivine'),('Finneon'),
 ('Lumineon'),('Mantyke'),('Snover'),('Abomasnow'),('Weavile'),('Magnezone'),('Lickilicky'),('Rhyperior'),('Tangrowth'),('Electivire'),('Magmortar'),('Togekiss'),('Yanmega'),
 ('Leafeon'),('Glaceon'),('Gliscor'),('Mamoswine'),('PorygonZ'),('Gallade'),('Probopass'),('Dusknoir'),('Froslass'),('Rotom'),('Uxie'),('Mesprit'),('Azelf'),('Dialga'),('Palkia'),
-('Heatran'),('Regigigas'),('Giratina'),('Cresselia'),('Phione'),('Manaphy'),('Darkrai'),('Shaymin'),('Arceus')
+('Heatran'),('Regigigas'),('Giratina'),('Cresselia'),('Phione'),('Manaphy'),('Darkrai'),('Shaymin'),('Arceus'),('Victini'),('Snivy'),('Servine'),('Serperior'),('Tepig'),
+('Pignite'),('Emboar'),('Oshawott'),('Dewott'),('Samurott'),('Patrat'),('Watchog'),('Lillipup'),('Herdier'),('Stoutland'),('Purrloin'),('Liepard'),('Pansage'),('Simisage'),
+('Pansear'),('Simisear'),('Panpour'),('Simipour'),('Munna'),('Musharna'),('Pidove'),('Tranquill'),('Unfezant'),('Blitzle'),('Zebstrika'),('Roggenrola'),('Boldore'),('Gigalith'),
+('Woobat'),('Swoobat'),('Drilbur'),('Excadrill'),('Audino'),('Timburr'),('Gurdurr'),('Conkeldurr'),('Tympole'),('Palpitoad'),('Seismitoad'),('Throh'),('Sawk'),('Sewaddle'),
+('Swadloon'),('Leavanny'),('Venipede'),('Whirlipede'),('Scolipede'),('Cottonee'),('Whimsicott'),('Petilil'),('Lilligant'),('Basculin'),('Sandile'),('Krokorok'),('Krookodile'),
+('Darumaka'),('Darmanitan'),('Maractus'),('Dwebble'),('Crustle'),('Scraggy'),('Scrafty'),('Sigilyph'),('Yamask'),('Cofagrigus'),('Tirtouga'),('Carracosta'),('Archen'),
+('Archeops'),('Trubbish'),('Garbodor'),('Zorua'),('Zoroark'),('Minccino'),('Cinccino'),('Gothita'),('Gothorita'),('Gothitelle'),('Solosis'),('Duosion'),('Reuniclus'),('Ducklett'),
+('Swanna'),('Vanillite'),('Vanillish'),('Vanilluxe'),('Deerling'),('Sawsbuck'),('Emolga'),('Karrablast'),('Escavalier'),('Foongus'),('Amoonguss'),('Frillish'),('Jellicent'),
+('Alomomola'),('Joltik'),('Galvantula'),('Ferroseed'),('Ferrothorn'),('Klink'),('Klang'),('Klinklang'),('Tynamo'),('Eelektrik'),('Eelektross'),('Elgyem'),('Beheeyem'),('Litwick'),
+('Lampent'),('Chandelure'),('Axew'),('Fraxure'),('Haxorus'),('Cubchoo'),('Beartic'),('Cryogonal'),('Shelmet'),('Accelgor'),('Stunfisk'),('Mienfoo'),('Mienshao'),('Druddigon'),
+('Golett'),('Golurk'),('Pawniard'),('Bisharp'),('Bouffalant'),('Rufflet'),('Braviary'),('Vullaby'),('Mandibuzz'),('Heatmor'),('Durant'),('Deino'),('Zweilous'),('Hydreigon'),
+('Larvesta'),('Volcarona'),('Cobalion'),('Terrakion'),('Virizion'),('Tornadus'),('Thundurus'),('Reshiram'),('Zekrom'),('Landorus'),('Kyurem'),('Keldeo'),('Meloetta'),('Genesect')
 
 
 
