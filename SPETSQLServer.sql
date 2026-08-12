dc301d54236fe0ac8ac54bf02cd419ca1ea0931e
rodrigomartins25 <rodrigo.martins25@univille.br>
2026-08-12 18:23:47 -0300

    Segunda Geração
    Adicionada toda a segunda geração de Pokémon (152-251) na tabela da National Dex

--- SPETSQLServer.sql
+++ SPETSQLServer.sql
@@ -71,8 +71,15 @@ VALUES ('Bulbasaur'),('Ivysaur'),('Venusaur'),('Charmander'),('Charmeleon'),('Ch
 ('Drowzee'),('Hypno'),('Krabby'),('Kingler'),('Voltorb'),('Electrode'),('Exeggcute'),('Exeggutor'),('Cubone'),('Marowak'),('Hitmonlee'),('Hitmonchan'),('Lickitung'),
 ('Koffing'),('Weezing'),('Rhyhorn'),('Rhydon'),('Chansey'),('Tangela'),('Kangaskhan'),('Horsea'),('Seadra'),('Goldeen'),('Seaking'),('Staryu'),('Starmie'),('Mr.Mime'),
 ('Scyther'),('Jynx'),('Electabuzz'),('Magmar'),('Pinsir'),('Tauros'),('Magikarp'),('Gyarados'),('Lapras'),('Ditto'),('Eevee'),('Vaporeon'),('Jolteon'),('Flareon'),('Porygon'),
-('Omanyte'),('Omastar'),('Kabuto'),('Kabutops'),('Aerodactyl'),('Snorlax'),('Articuno'),('Zapdos'),('Moltres'),('Dratini'),('Dragonair'),('Dragonite'),('Mewtwo'),('Mew')
-
+('Omanyte'),('Omastar'),('Kabuto'),('Kabutops'),('Aerodactyl'),('Snorlax'),('Articuno'),('Zapdos'),('Moltres'),('Dratini'),('Dragonair'),('Dragonite'),('Mewtwo'),('Mew'),
+('Chikorita'),('Bayleef'),('Meganium'),('Cyndaquil'),('Quilava'),('Typhlosion'),('Totodile'),('Croconaw'),('Feraligatr'),('Sentret'),('Furret'),('Hoothoot'),('Noctowl'),
+('Ledyba'),('Ledian'),('Spinarak'),('Ariados'),('Crobat'),('Chinchou'),('Lanturn'),('Pichu'),('Cleffa'),('Igglybuff'),('Togepi'),('Togetic'),('Natu'),('Xatu'),('Mareep'),
+('Flaaffy'),('Ampharos'),('Bellossom'),('Marill'),('Azumarill'),('Sudowoodo'),('Politoed'),('Hoppip'),('Skiploom'),('Jumpluff'),('Aipom'),('Sunkern'),('Sunflora'),('Yanma'),
+('Wooper'),('Quagsire'),('Espeon'),('Umbreon'),('Murkrow'),('Slowking'),('Misdreavus'),('Unown'),('Wobbuffet'),('Girafarig'),('Pineco'),('Forretress'),('Dunsparce'),
+('Gligar'),('Steelix'),('Snubbull'),('Granbull'),('Qwilfish'),('Scizor'),('Shuckle'),('Heracross'),('Sneasel'),('Teddiursa'),('Ursaring'),('Slugma'),('Magcargo'),('Swinub'),
+('Piloswine'),('Corsola'),('Remoraid'),('Octillery'),('Delibird'),('Mantine'),('Skarmory'),('Houndour'),('Houndoom'),('Kingdra'),('Phanpy'),('Donphan'),('Porygon2'),
+('Stantler'),('Smeargle'),('Tyrogue'),('Hitmontop'),('Smoochum'),('Elekid'),('Magby'),('Miltank'),('Blissey'),('Raikou'),('Entei'),('Suicune'),('Larvitar'),('Pupitar'),
+('Tyranitar'),('Lugia'),('Ho-Oh'),('Celebi')
 
 INSERT LocationsHoenn (locationName)
 VALUES ('Route 101'),('Route 102'),('Route 103'),('Route 104'),('Route 105'),('Route 106'),('Route 107'),('Route 108'),('Route 109'),('Route 110'),('Route 111'),('Route 112'),('Route 113'),
