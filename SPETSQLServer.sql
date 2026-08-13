2b610ef62066bac772b30bab6fc20b328b697c8b
rodrigodesamartins0 <rodrigodesamartins0@gmail.com>
2026-08-12 21:58:40 -0300

    Sétima Geração (+Meltan)
    Sétima geração e Meltan/Melmetal (#722-#809) adicionados a tabela da National Pokédex

--- SPETSQLServer.sql
+++ SPETSQLServer.sql
@@ -69,7 +69,7 @@ VALUES ('Bulbasaur'),('Ivysaur'),('Venusaur'),('Charmander'),('Charmeleon'),('Ch
 ('Machamp'),('Bellsprout'),('Weepinbell'),('Victreebel'),('Tentacool'),('Tentacruel'),('Geodude'),('Graveler'),('Golem'),('Ponyta'),('Rapidash'),('Slowpoke'),('Slowbro'),
 ('Magnemite'),('Magneton'),('Farfetchd'),('Doduo'),('Dodrio'),('Seel'),('Dewgong'),('Grimer'),('Muk'),('Shellder'),('Cloyster'),('Gastly'),('Haunter'),('Gengar'),('Onix'),
 ('Drowzee'),('Hypno'),('Krabby'),('Kingler'),('Voltorb'),('Electrode'),('Exeggcute'),('Exeggutor'),('Cubone'),('Marowak'),('Hitmonlee'),('Hitmonchan'),('Lickitung'),
-('Koffing'),('Weezing'),('Rhyhorn'),('Rhydon'),('Chansey'),('Tangela'),('Kangaskhan'),('Horsea'),('Seadra'),('Goldeen'),('Seaking'),('Staryu'),('Starmie'),('Mr.Mime'),
+('Koffing'),('Weezing'),('Rhyhorn'),('Rhydon'),('Chansey'),('Tangela'),('Kangaskhan'),('Horsea'),('Seadra'),('Goldeen'),('Seaking'),('Staryu'),('Starmie'),('Mr. Mime'),
 ('Scyther'),('Jynx'),('Electabuzz'),('Magmar'),('Pinsir'),('Tauros'),('Magikarp'),('Gyarados'),('Lapras'),('Ditto'),('Eevee'),('Vaporeon'),('Jolteon'),('Flareon'),('Porygon'),
 ('Omanyte'),('Omastar'),('Kabuto'),('Kabutops'),('Aerodactyl'),('Snorlax'),('Articuno'),('Zapdos'),('Moltres'),('Dratini'),('Dragonair'),('Dragonite'),('Mewtwo'),('Mew'),
 ('Chikorita'),('Bayleef'),('Meganium'),('Cyndaquil'),('Quilava'),('Typhlosion'),('Totodile'),('Croconaw'),('Feraligatr'),('Sentret'),('Furret'),('Hoothoot'),('Noctowl'),
@@ -77,9 +77,9 @@ VALUES ('Bulbasaur'),('Ivysaur'),('Venusaur'),('Charmander'),('Charmeleon'),('Ch
 ('Flaaffy'),('Ampharos'),('Bellossom'),('Marill'),('Azumarill'),('Sudowoodo'),('Politoed'),('Hoppip'),('Skiploom'),('Jumpluff'),('Aipom'),('Sunkern'),('Sunflora'),('Yanma'),
 ('Wooper'),('Quagsire'),('Espeon'),('Umbreon'),('Murkrow'),('Slowking'),('Misdreavus'),('Unown'),('Wobbuffet'),('Girafarig'),('Pineco'),('Forretress'),('Dunsparce'),
 ('Gligar'),('Steelix'),('Snubbull'),('Granbull'),('Qwilfish'),('Scizor'),('Shuckle'),('Heracross'),('Sneasel'),('Teddiursa'),('Ursaring'),('Slugma'),('Magcargo'),('Swinub'),
-('Piloswine'),('Corsola'),('Remoraid'),('Octillery'),('Delibird'),('Mantine'),('Skarmory'),('Houndour'),('Houndoom'),('Kingdra'),('Phanpy'),('Donphan'),('Porygon2'),
+('Piloswine'),('Corsola'),('Remoraid'),('Octillery'),('Delibird'),('Mantine'),('Skarmory'),('Houndour'),('Houndoom'),('Kingdra'),('Phanpy'),('Donphan'),('Porygon 2'),
 ('Stantler'),('Smeargle'),('Tyrogue'),('Hitmontop'),('Smoochum'),('Elekid'),('Magby'),('Miltank'),('Blissey'),('Raikou'),('Entei'),('Suicune'),('Larvitar'),('Pupitar'),
-('Tyranitar'),('Lugia'),('HoOh'),('Celebi'),('Treecko'),('Grovyle'),('Sceptile'),('Torchic'),('Combusken'),('Blaziken'),('Mudkip'),('Marshtomp'),('Swampert'),('Poochyena'),
+('Tyranitar'),('Lugia'),('Ho-Oh'),('Celebi'),('Treecko'),('Grovyle'),('Sceptile'),('Torchic'),('Combusken'),('Blaziken'),('Mudkip'),('Marshtomp'),('Swampert'),('Poochyena'),
 ('Mightyena'),('Zigzagoon'),('Linoone'),('Wurmple'),('Silcoon'),('Beautifly'),('Cascoon'),('Dustox'),('Lotad'),('Lombre'),('Ludicolo'),('Seedot'),('Nuzleaf'),('Shiftry'),
 ('Taillow'),('Swellow'),('Wingull'),('Pelipper'),('Ralts'),('Kirlia'),('Gardevoir'),('Surskit'),('Masquerain'),('Shroomish'),('Breloom'),('Slakoth'),('Vigoroth'),('Slaking'),
 ('Nincada'),('Ninjask'),('Shedinja'),('Whismur'),('Loudred'),('Exploud'),('Makuhita'),('Hariyama'),('Azurill'),('Nosepass'),('Skitty'),('Delcatty'),('Sableye'),('Mawile'),
@@ -95,7 +95,7 @@ VALUES ('Bulbasaur'),('Ivysaur'),('Venusaur'),('Charmander'),('Charmeleon'),('Ch
 ('Mismagius'),('Honchkrow'),('Glameow'),('Purugly'),('Chingling'),('Stunky'),('Skuntank'),('Bronzor'),('Bronzong'),('Bonsly'),('MimeJr.'),('Happiny'),('Chatot'),('Spiritomb'),
 ('Gible'),('Gabite'),('Garchomp'),('Munchlax'),('Riolu'),('Lucario'),('Hippopotas'),('Hippowdon'),('Skorupi'),('Drapion'),('Croagunk'),('Toxicroak'),('Carnivine'),('Finneon'),
 ('Lumineon'),('Mantyke'),('Snover'),('Abomasnow'),('Weavile'),('Magnezone'),('Lickilicky'),('Rhyperior'),('Tangrowth'),('Electivire'),('Magmortar'),('Togekiss'),('Yanmega'),
-('Leafeon'),('Glaceon'),('Gliscor'),('Mamoswine'),('PorygonZ'),('Gallade'),('Probopass'),('Dusknoir'),('Froslass'),('Rotom'),('Uxie'),('Mesprit'),('Azelf'),('Dialga'),('Palkia'),
+('Leafeon'),('Glaceon'),('Gliscor'),('Mamoswine'),('Porygon-Z'),('Gallade'),('Probopass'),('Dusknoir'),('Froslass'),('Rotom'),('Uxie'),('Mesprit'),('Azelf'),('Dialga'),('Palkia'),
 ('Heatran'),('Regigigas'),('Giratina'),('Cresselia'),('Phione'),('Manaphy'),('Darkrai'),('Shaymin'),('Arceus'),('Victini'),('Snivy'),('Servine'),('Serperior'),('Tepig'),
 ('Pignite'),('Emboar'),('Oshawott'),('Dewott'),('Samurott'),('Patrat'),('Watchog'),('Lillipup'),('Herdier'),('Stoutland'),('Purrloin'),('Liepard'),('Pansage'),('Simisage'),
 ('Pansear'),('Simisear'),('Panpour'),('Simipour'),('Munna'),('Musharna'),('Pidove'),('Tranquill'),('Unfezant'),('Blitzle'),('Zebstrika'),('Roggenrola'),('Boldore'),('Gigalith'),
@@ -113,7 +113,13 @@ VALUES ('Bulbasaur'),('Ivysaur'),('Venusaur'),('Charmander'),('Charmeleon'),('Ch
 ('Espurr'),('Meowstic'),('Honedge'),('Doublade'),('Aegislash'),('Spritzee'),('Aromatisse'),('Swirlix'),('Slurpuff'),('Inkay'),('Malamar'),('Binacle'),('Barbaracle'),('Skrelp'),
 ('Dragalge'),('Clauncher'),('Clawitzer'),('Helioptile'),('Heliolisk'),('Tyrunt'),('Tyrantrum'),('Amaura'),('Aurorus'),('Sylveon'),('Hawlucha'),('Dedenne'),('Carbink'),('Goomy'),
 ('Sliggoo'),('Goodra'),('Klefki'),('Phantump'),('Trevenant'),('Pumpkaboo'),('Gourgeist'),('Bergmite'),('Avalugg'),('Noibat'),('Noivern'),('Xerneas'),('Yveltal'),('Zygarde'),
-('Diancie'),('Hoopa'),('Volcanion')
+('Diancie'),('Hoopa'),('Volcanion'),('Rowlet'),('Dartrix'),('Decidueye'),('Litten'),('Torracat'),('Incineroar'),('Popplio'),('Brionne'),('Primarina'),('Pikipek'),('Trumbeak'),
+('Toucannon'),('Yungoos'),('Gumshoos'),('Grubbin'),('Charjabug'),('Vikavolt'),('Crabrawler'),('Crabominable'),('Oricorio'),('Cutiefly'),('Ribombee'),('Rockruff'),('Lycanroc'),
+('Wishiwashi'),('Mareanie'),('Toxapex'),('Mudbray'),('Mudsdale'),('Dewpider'),('Araquanid'),('Fomantis'),('Lurantis'),('Morelull'),('Shiinotic'),('Salandit'),('Salazzle'),
+('Stufful'),('Bewear'),('Bounsweet'),('Steenee'),('Tsareena'),('Comfey'),('Oranguru'),('Passimian'),('Wimpod'),('Golisopod'),('Sandygast'),('Palossand'),('Pyukumuku'),
+('Type:Null'),('Silvally'),('Minior'),('Komala'),('Turtonator'),('Togedemaru'),('Mimikyu'),('Bruxish'),('Drampa'),('Dhelmise'),('Jangmo-o'),('Hakamo-o'),('Kommo-o'),('Tapu Koko'),
+('Tapu Lele'),('Tapu Bulu'),('Tapu Fini'),('Cosmog'),('Cosmoem'),('Solgaleo'),('Lunala'),('Nihilego'),('Buzzwole'),('Pheromosa'),('Xurkitree'),('Celesteela'),('Kartana'),('Guzzlord'),
+('Necrozma'),('Magearna'),('Marshadow'),('Poipole'),('Naganadel'),('Stakataka'),('Blacephalon'),('Zeraora'),('Meltan'),('Melmetal')
 
 
 
